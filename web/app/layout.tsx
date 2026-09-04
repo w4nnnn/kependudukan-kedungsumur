dd99b6848aaec9032f80a3d44ead8defcc95693b
w4nnnn <irwanfebriansyah112@gmail.com>
2026-09-04 15:15:36 +0700

    feat(web): add and mount sonner toaster component

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,6 +4,7 @@ import { Fira_Code, Fira_Sans } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { TooltipProvider } from "@/components/ui/tooltip"
+import { Toaster } from "@/components/ui/sonner"
 
 const fontSans = Fira_Sans({
   weight: ["300", "400", "500", "600", "700"],
@@ -64,6 +65,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider>
           <TooltipProvider>{children}</TooltipProvider>
+          <Toaster position="top-right" richColors closeButton />
         </ThemeProvider>
       </body>
     </html>
